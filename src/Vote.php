61e3db51fb5db6951e5ce52e6f236ae3e042657d
Brian Faust <hello@brianfaust.de>
2016-12-02 19:21:22 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Vote.php
+++ src/Vote.php
@@ -53,7 +53,7 @@ class Vote extends Model
     {
         $query = $voteable->votes();
 
-        if (! empty($to)) {
+        if (!empty($to)) {
             $range = [new Carbon($from), new Carbon($to)];
         } else {
             $range = [
@@ -83,7 +83,7 @@ class Vote extends Model
 
     protected static function cast(Model $voteable, $value = 1)
     {
-        if (! $voteable->exists) {
+        if (!$voteable->exists) {
             return false;
         }
 
