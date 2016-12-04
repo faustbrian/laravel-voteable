3f43cc10a243b68b5b6ee447f20700076b314fbb
Brian Faust <hello@brianfaust.de>
2016-12-04 09:11:16 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Vote.php
+++ src/Vote.php
@@ -24,11 +24,7 @@ namespace BrianFaust\Voteable;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Vote extends Model
 {
